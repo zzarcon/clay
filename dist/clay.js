a1aeea8afc989a189b3aec44527a9b315929c8ae
Hector Leon Zarco Garcia <hector.zarco@king.com>
2015-08-05 18:40:39 +0200

    v0.1.2

--- dist/clay.js
+++ dist/clay.js
@@ -1,13 +1,17 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.clay = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
 var _utils = require("./utils");
 
-var elementResizeEvent = require('element-resize-event');
+var _elementResizeEvent = require('element-resize-event');
+
+var _elementResizeEvent2 = _interopRequireDefault(_elementResizeEvent);
 
 exportModule('Clay', function () {
   var Clay = (function () {
@@ -47,7 +51,7 @@ exportModule('Clay', function () {
     _createClass(Clay, [{
       key: 'addEvents',
       value: function addEvents() {
-        elementResizeEvent(this.el, (function () {
+        (0, _elementResizeEvent2['default'])(this.el, (function () {
           var cb = this.eventHanlers['resize'];
           if (!cb) return;
 
@@ -232,4 +236,5 @@ module.exports = {
   raf: raf
 };
 
-},{}]},{},[1]);
+},{}]},{},[1])(1)
+});
